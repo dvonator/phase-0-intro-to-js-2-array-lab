84480e7726374e029741d9c153b433f27fb60363
David <davidvo1992@gmail.com>
2023-02-14 14:08:37 -0600

    commit tree 76374373fa83378a2542c0189f2f5d6e18617048

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  const CopyofCats = [...cats];
+  CopyofCats.push(name);
+  return CopyofCats;
+}
+
+function prependCat(name) {
+  const CopyofCats = [...cats];
+  CopyofCats.unshift(name);
+  return CopyofCats;
+}
+
+function removeLastCat(name) {
+  const CopyofCats = [...cats];
+  CopyofCats.pop(name);
+  return CopyofCats;
+}
+
+function removeFirstCat(name) {
+  const CopyofCats = [...cats];
+  CopyofCats.shift(name);
+  return CopyofCats;
+}
